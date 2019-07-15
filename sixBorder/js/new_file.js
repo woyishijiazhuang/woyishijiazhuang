d692e36333a742f7fdff3eb98db13f53752e1a27
woyishijiazhuang <44568291+woyishijiazhuang@users.noreply.github.com>
2019-07-15 20:07:37 +0800

    Add files via upload

---
+++ sixBorder/js/new_file.js
@@ -0,0 +1,19 @@
+function test(){
+	
+}
+
+function suofang(){
+	var img = document.getElementById("img");
+	img.style.width = "120%";
+	img.style.height="120%";
+	img.style.position="absolute";
+	img.style.left="-10%";
+	img.style.top="-10%";
+}
+function huifu(){
+	var img = document.getElementById("img");
+	img.style.width = "100%";
+	img.style.height="100%";
+	img.style.left="";
+	img.style.top="";
+}
